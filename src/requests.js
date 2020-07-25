b58cf4670e96a020d92124e2ef8e71187e68d5b7
muNeves3 <murilogrilosn@gmail.com>
2020-07-25 16:55:15 -0300

    calling the api

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "dfca9c4d01546e2c6f2196c086d78bd3";
+
+/**final of the url requests */
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with-genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with-genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with-genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with-genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with-genres=99`,
+};
+
+export default requests;
