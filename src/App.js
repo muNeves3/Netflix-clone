e19df8f04e46aec13e0c969a93541a3126b76aed
muNeves3 <murilogrilosn@gmail.com>
2020-07-26 17:03:29 -0300

    Calling the other requests

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import "./App.css";
 
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey, Murilo</h1>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    <div className="app">
+      {/* Banner */}
+      {/* NavBar */}
+      <Banner />
       <Row
-        title="Netflix originals"
+        title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Science Fiction" fetchUrl={requests.fetchScienceFic} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Dramas" fetchUrl={requests.fetchDramaMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Most Popular" fetchUrl={requests.fetchMostPopular} />
     </div>
   );
 }
