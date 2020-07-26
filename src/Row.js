e0a594506ea72fc3c2812ffdf6a8097e49c3bfca
muNeves3 <murilogrilosn@gmail.com>
2020-07-26 17:06:05 -0300

    New prop:isLargeRow

--- src/Row.js
+++ src/Row.js
@@ -5,7 +5,7 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code wich runs based on a specific condition/variable, in this case, when the row loads
@@ -19,8 +19,6 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.table(movies);
-
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -29,8 +27,11 @@ function Row({ title, fetchUrl }) {
 
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
