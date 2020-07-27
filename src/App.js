20814ad0c457a40dacf47d343d5d730358379af1
muNeves3 <murilogrilosn@gmail.com>
2020-07-27 12:59:41 -0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-      {/* Banner */}
-      {/* NavBar */}
+      <Nav />
       <Banner />
       <Row
         title="Netflix Originals"
